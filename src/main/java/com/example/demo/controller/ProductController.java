86513603c50502984961db3951b6b11e24c07d75
Violet <daby6789@gmail.com>
2024-09-03 10:05:32 +0800

    add exception handler and debug

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -10,18 +10,39 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("")
 public class ProductController {
 
     private static final ProductService productService = new ProductService();
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductVO> getProduct(@PathVariable("id") String productId) {
-        var product = productService.getProductVO(productId);
-        return ResponseEntity.ok(product);
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductResVo> getProduct(@PathVariable("id") String productId) {
+        System.out.println("=========== Start ProductController.getProduct ============");
+
+        ResponseEntity<ProductResVo> result = null;
+        try {
+            var product = productService.getProductVO(productId);
+
+
+            ProductResVo req = new ProductResVo();
+            req.setCode(200);
+            req.setMessage("Success");
+            result = ResponseEntity.ok(req);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ProductResVo req = new ProductResVo();
+            req.setCode(404);
+            req.setMessage(e.getMessage());
+            result = ResponseEntity.ok(req);
+        } finally {
+            System.out.println("=========== End ProductController.getProduct ============");
+        }
+        return result;
     }
 
     @PostMapping("")
@@ -53,12 +74,22 @@ public class ProductController {
     }
 
 
-    @GetMapping("")
+    @GetMapping("/products")
     public ResponseEntity<List<ProductVO>> getProducts(
             @ModelAttribute ProductRequestParameter param
     ) {
-        var products = productService.getProductVOs(param);
-        return ResponseEntity.ok(products);
+        System.out.println("=========== Start ProductController.getProducts ============");
+
+        try {
+            var products = productService.getProductVOs(param);
+            return ResponseEntity.ok(products);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        } finally {
+            System.out.println("=========== End ProductController.getProducts ============");
+        }
+
+        return null;
 
     }
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -8,6 +8,7 @@ import com.example.demo.repository.ProductRepository;
 import com.example.demo.repository.UserRepository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ProductService {
@@ -28,48 +29,68 @@ public class ProductService {
     }
 
     public ProductVO getProductVO(String id) {
-        var productPO = getProductPO(id);
-        var productVO = ProductVO.of(productPO);
+        System.out.println("=========== Start ProductService.getProductVO ============");
 
-        var user = userRepository.getOneById(productPO.getCreatorId());
-        productVO.setCreatorName(user.getName());
+        ProductVO productVO = null;
+        try {
+            ProductPO productPO = productRepository.getOneById(id);
+            if (Objects.isNull(productPO)) {
+                throw new NotFoundException("Product " + id + "doesn't exist.");
+            }
+
+            productVO = ProductVO.of(productPO);
+
+
+            var user = userRepository.getOneById(productPO.getCreatorId());
+
+            productVO.setCreatorName(user.getName());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            System.out.println("=========== End ProductService.getProductVO ============");
+        }
+
 
         return productVO;
     }
 
-    private ProductPO getProductPO(String id) {
-        var po = productRepository.getOneById(id);
-        if (po == null) {
-            throw new NotFoundException("Product " + id + "doesn't exist.");
-        }
-        return po;
-    }
-
     public List<ProductVO> getProductVOs(ProductRequestParameter param) {
-        var productPOs = productRepository.getMany(param);
-        var userIds = productPOs.stream().map(ProductPO::getCreatorId).toList();
-        var userIdNameMap = userRepository.getManyByIds(userIds)
-                .stream()
-                .collect(Collectors.toMap(UserPO::getId, UserPO::getName));
-        return productPOs.stream()
-                .map(po ->
-                {
-                    var vo = ProductVO.of(po);
-                    vo.setCreatorName(userIdNameMap.get(vo.getCreatorId()));
-                    return vo;
-                }).toList();
+        System.out.println("=========== Start ProductService.getProductVOs ============");
+
+        try {
+            var productPOs = productRepository.getMany(param);
+            var userIds = productPOs.stream().map(ProductPO::getCreatorId).toList();
+            var userIdNameMap = userRepository.getManyByIds(userIds)
+                    .stream()
+                    .collect(Collectors.toMap(UserPO::getId, UserPO::getName));
+
+            return productPOs.stream()
+                    .map(po ->
+                    {
+                        var vo = ProductVO.of(po);
+                        vo.setCreatorName(userIdNameMap.get(vo.getCreatorId()));
+                        return vo;
+                    }).toList();
+        } catch (Exception e) {
+            System.out.println("Error");
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("=========== End ProductService.getProductVOs ============");
+        }
+        return null;
 
     }
 
     public void updateProduct(String id, ProductUpdateRequest productReq) {
-        var productPO = getProductPO(id);
+        var productPO = productRepository.getOneById(id);
         productPO.setName(productReq.getName());
         productPO.setPrice(productReq.getPrice());
         productRepository.update(productPO);
     }
 
     public void deleteProduct(String id) {
-        var productPO = getProductPO(id);
+        var productPO = productRepository.getOneById(id);
         productRepository.deleteById(productPO.getId());
     }
 
