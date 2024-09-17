e130f824330e4ee97bc6672f552cf8293082b93a
Violet <daby6789@gmail.com>
2024-09-17 22:59:52 +0800

    use @EntityGraph to solve N+1 problem

--- src/main/java/com/violet/demo/repository/StudentRepository.java
+++ src/main/java/com/violet/demo/repository/StudentRepository.java
@@ -1,10 +1,12 @@
 package com.violet.demo.repository;
 
 import com.violet.demo.model.entity.Student;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @EntityGraph(attributePaths = "contact")
     List<Student> findByNameLikeIgnoreCase(String name);
 }
