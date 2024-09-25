1d0dc2088a29a981e08117c4744b87ad2e0dbf18
Violet <daby6789@gmail.com>
2024-09-25 10:04:19 +0800

    lesson 14-3 end

--- src/main/java/com/violet/demo/config/MailConfig.java
+++ src/main/java/com/violet/demo/config/MailConfig.java
@@ -1,15 +1,44 @@
 package com.violet.demo.config;
 
+import com.violet.demo.service.MailService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MailConfig {
     @Value("${mail.display-name}")
     private String displayName;
 
-    @Value("${spring.mail.username}")
+    @Value("${spring.mail.gmail.username}")
     private String userName;
+
+    @Value("${spring.mail.gmail.host}")
+    private String gmailHost;
+
+    @Value("${spring.mail.gmail.port}")
+    private int gmailPort;
+
+    @Value("${spring.mail.gmail.username}")
+    private String gmailUsername;
+
+    @Value("${spring.mail.gmail.password}")
+    private String gmailPassword;
+
+    @Value("${spring.mail.gmail.properties.mail.smtp.auth}")
+    private Boolean gmailSmtpAuth;
+
+    @Value("${spring.mail.gmail.properties.mail.smtp.starttls.enable}")
+    private Boolean gmailSmtpTlsEnable;
+
+    @Value("${spring.mail.gmail.properties.mail.transport.protocol}")
+    private String gmailTransProtocol;
 
     public String getDisplayName() {
         return displayName;
@@ -17,5 +46,22 @@ public class MailConfig {
 
     public String getUserName() {
         return userName;
+    }
+
+
+    @Bean
+    public MailService mailService() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(gmailHost);
+        sender.setPort(gmailPort);
+        sender.setUsername(gmailUsername);
+        sender.setPassword(gmailPassword);
+
+        Properties props = sender.getJavaMailProperties();
+        props.put("mail.smtp.auth", gmailSmtpAuth); //是否向郵件服務驗證身份
+        props.put("mail.smtp.starttls.enable", gmailSmtpTlsEnable); //是否啟用 TLS(傳輸層安全)，對通訊加密
+        props.put("mail.transport.protocol", gmailTransProtocol); //傳輸協定
+
+        return new MailService(sender);
     }
 }
